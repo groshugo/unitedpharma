e417f19e15f3afa58eb98ee09a440130c6f77982
huydan2k <huydan2k@gmail.com>
2012-04-08 18:30:12 +0000

    Add column NOteOfSalesmen to Customer and CustomerLog table

---
+++ 01_DOCUMENT/Database/QueryFiles/Add_Note_Col_To_Customer_CustomerLog.sql
@@ -0,0 +1,10 @@
+ALTER TABLE [UPI].[dbo].[Customer]
+ADD [NoteOfSalesmen] nvarchar(4000) null;
+ 
+ Go
+ 
+ ALTER TABLE [UPI].[dbo].[CustomerLog]
+ADD [NoteOfSalesmen] nvarchar(4000) null;
+ 
+ Go
+ 
